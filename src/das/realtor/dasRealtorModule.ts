66d981a83af5300a22789ce118676c9a67ab349b
J1032 <J1032@example.com>
2023-04-14 07:13:04 -0600

    Added realtor query api

--- src/das/realtor/dasRealtorModule.ts
+++
@@ -1,1 +0,0 @@
-import { Module } from '@nestjs/common';import { ConfigModule } from '@nestjs/config';import { DasRealtorRepository } from './das-realtor-repository.service';import { DasRealtorController } from './dasRealtorController';@Module({  imports: [ConfigModule],  controllers: [DasRealtorController],  providers: [DasRealtorRepository],})export class DasRealtorModule {}
---
+++ src/das/realtor/realtor.module.ts
@@ -0,0 +1,1 @@
+import { Module } from '@nestjs/common';import { ConfigModule } from '@nestjs/config';import { RealtorController } from './realtor.controller';import { RealtorRepository } from './realtor.repository';@Module({  imports: [ConfigModule],  controllers: [RealtorController],  providers: [RealtorRepository],})export class RealtorModule {}
